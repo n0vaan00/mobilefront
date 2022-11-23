47b633bee4fc33f67471f2f50f3440229cb1de57
Anu Väyrynen <n0vaan00@students.oamk.fi>
2022-11-23 20:12:16 +0200

    errorrrii

--- components/Errorinfo.js
+++ components/Errorinfo.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { View, Text} from 'react-native';
+import { View, Text, Image } from 'react-native';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import styles from '../style/style';
+
 
 export default function Errorinfo() {
   return (
@@ -9,13 +10,8 @@ export default function Errorinfo() {
       <View style={styles.square}>
         <View style={styles.flex2}>
       <Text style={styles.errortext}>Hupsista jokin meni pieleen.</Text>
-      <Text><MaterialCommunityIcons
-                name={'alert-circle'}
-                color={'#ffffff'}
-                size={'200'}
-                style={styles.error}
-              ></MaterialCommunityIcons>
-      </Text>
+      <Image source = {require('../assets/sp3.png')}
+       />
       </View>
       </View>
     </View>
