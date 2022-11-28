63c25a2a1c97bd71bf919b66478099037b150641
adepade <n7haat00@students.oamk.fi>
2022-11-28 16:43:52 +0200

    spinner värit koot

--- components/Elepricenow.js
+++ components/Elepricenow.js
@@ -138,7 +138,7 @@ export default function Elepricenow() {
         <Text style={styles.title}>Sähkön hinta tänään (snt/kWh,sis. Alv 24%)</Text>
         <Text style={styles.flex}>
           <Text style={styles.text}>Hinta nyt:  </Text>
-            <Text style={styles.important}>{priceNow?priceNow : <ActivityIndicator size="large" />}       
+            <Text style={styles.important}>{priceNow?priceNow : <ActivityIndicator size="large" color="#ffffff"/>}       
               <MaterialCommunityIcons
                 name={'arrow-' + arrow + '-bold'}
                 color={color}
@@ -148,15 +148,15 @@ export default function Elepricenow() {
             </Text>
         <Text style={styles.flex}>
           <Text style={styles.text}>Päivän ylin:  </Text>
-          <Text style={styles.notimportant}>{maxPrice?maxPrice : <ActivityIndicator size="large" />} </Text>
+          <Text style={styles.notimportant}>{maxPrice?maxPrice : <ActivityIndicator size="small" color="#ffffff"/>} </Text>
         </Text>
         <Text style={styles.flex}>
           <Text style={styles.text}>Päivän alin:  </Text>
-          <Text style={styles.notimportant}>{minPrice?minPrice : <ActivityIndicator size="large" />}</Text>
+          <Text style={styles.notimportant}>{minPrice?minPrice : <ActivityIndicator size="small" color="#ffffff"/>}</Text>
         </Text>
           <Text style={styles.flex}>
             <Text style={styles.text}>Päivän keskihinta:  </Text>
-            <Text style={styles.notimportant}>{avg?avg : <ActivityIndicator size="large" />}</Text>
+            <Text style={styles.notimportant}>{avg?avg : <ActivityIndicator size="small" color="#ffffff"/>}</Text>
           </Text>
           </ScrollView>
         </View>
